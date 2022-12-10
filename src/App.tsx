c1a1258a0946f22c1069abe84a77396cc2bb8a91
FelipeMondragon <luis.felipe.mondragon@proton.me>
2022-12-10 12:26:27 -0600

    primer comentario

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  let texto = "";
+  const imagenURL =
+    "https://blog.wildix.com/wp-content/uploads/2020/06/react-logo.jpg";
+
+  const manejarClick = () => console.log("click");
+
+  const manejarKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    console.log(e.currentTarget.value);
+    texto = e.currentTarget.value;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="rojo">Hola mundo!</h1>
+      <button
+        onMouseEnter={() => console.log("entrando")}
+        onClick={() => console.log("click desde el botón")}
+      >
+        Clickeame
+      </button>
+      <br />
+      <br />
+      <input type="text" onKeyUp={(e) => manejarKeyUp(e)} />
+      <div> {texto} </div>
+      <br />
+      <br />
+      <img alt="logo react" src={imagenURL} />
+      <div>
+        <input type="checkbox" checked={true} /> Este es un checkbox
+      </div>
+    </>
   );
 }
 
